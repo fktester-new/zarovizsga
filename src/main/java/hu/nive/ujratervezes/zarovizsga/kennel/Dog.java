dd4603a3e14805e557563fb560ce02d387092ace
fktester <kofe16@gmail.com>
2021-03-16 19:36:25 +0100

    Kennel

---
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/kennel/Dog.java
@@ -0,0 +1,24 @@
+package hu.nive.ujratervezes.zarovizsga.kennel;
+
+public abstract class Dog {
+
+    String name;
+    int happiness;
+
+    public Dog(String name) {
+        this.name = name;
+        happiness = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHappiness() {
+        return happiness;
+    }
+
+    abstract void feed();
+
+    abstract void play(int hours);
+}
