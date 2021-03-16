17da6369b815c3bb8ea688ffba69a2815e7f3d99
fktester <kofe16@gmail.com>
2021-03-16 17:56:00 +0100

    DigitsCounter

--- src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
@@ -1,4 +1,36 @@
 package hu.nive.ujratervezes.zarovizsga.digitscounter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class DigitsCounter {
+
+    public int getCountOfDigits(String s){
+        Set<Integer> result = new HashSet<>();
+        if (! isEmpty(s)){
+            String[] chars = s.split("");
+            for (int i = 0; i < chars.length; i++){
+                if (isNumber(chars[i])){
+                    result.add(Integer.parseInt(chars[i]));
+                }
+            }
+            return result.size();
+        } else {
+            return 0;
+        }
+
+    }
+
+    public boolean isEmpty(String s){
+        return s == null || "".equals(s);
+    }
+
+    private boolean isNumber(String s) {
+        try {
+            Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+    }
 }
--- src/test/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounterTest.java
+++ src/test/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounterTest.java
@@ -2,6 +2,8 @@ package hu.nive.ujratervezes.zarovizsga.digitscounter;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class DigitsCounterTest {
 
     @Test
