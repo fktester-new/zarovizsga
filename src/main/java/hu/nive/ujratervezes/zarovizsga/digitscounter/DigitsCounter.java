da6985d16733e6a0b618a894a178325c60e09491
fktester <kofe16@gmail.com>
2021-03-19 22:57:37 +0100

    DigitsCounter

--- src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
@@ -5,12 +5,12 @@ import java.util.Set;
 
 public class DigitsCounter {
 
-    public int getCountOfDigits(String s){
+    public int getCountOfDigits(String s) {
         Set<Integer> result = new HashSet<>();
-        if (! isEmpty(s)){
+        if (!isEmpty(s)) {
             String[] chars = s.split("");
-            for (int i = 0; i < chars.length; i++){
-                if (isNumber(chars[i])){
+            for (int i = 0; i < chars.length; i++) {
+                if (isNumber(chars[i])) {
                     result.add(Integer.parseInt(chars[i]));
                 }
             }
@@ -21,7 +21,22 @@ public class DigitsCounter {
 
     }
 
-    public boolean isEmpty(String s){
+    public int getCountOfDigitsWithoutSplit(String s) {
+        Set<Character> result = new HashSet<>();
+        if (isEmpty(s)) {
+            return 0;
+        } else {
+            char[] chars = s.toCharArray();
+            for (char c : chars) {
+                if (Character.isDigit(c)) {
+                    result.add(c);
+                }
+            }
+            return result.size();
+        }
+    }
+
+    public boolean isEmpty(String s) {
         return s == null || "".equals(s);
     }
 
--- src/test/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounterTest.java
+++ src/test/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounterTest.java
@@ -17,4 +17,16 @@ class DigitsCounterTest {
         assertEquals(3, digitsCounter.getCountOfDigits("1212123123123123"));
         assertEquals(3, digitsCounter.getCountOfDigits("12asd 12ewr1q2breg31 rw23123123"));
     }
+
+    @Test
+    void getCountOfDigitsWithoutSplit() {
+        DigitsCounter digitsCounter = new DigitsCounter();
+
+        assertEquals(0, digitsCounter.getCountOfDigitsWithoutSplit(null));
+        assertEquals(0, digitsCounter.getCountOfDigitsWithoutSplit(""));
+        assertEquals(1, digitsCounter.getCountOfDigitsWithoutSplit("1"));
+        assertEquals(2, digitsCounter.getCountOfDigitsWithoutSplit("111112222"));
+        assertEquals(3, digitsCounter.getCountOfDigitsWithoutSplit("1212123123123123"));
+        assertEquals(3, digitsCounter.getCountOfDigitsWithoutSplit("12asd 12ewr1q2breg31 rw23123123"));
+    }
 }
